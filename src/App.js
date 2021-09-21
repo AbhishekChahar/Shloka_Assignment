26b9c5c9c22c1df074e8f4167e539b3c728a6060
AbhishekChahar <abhishekchaharr@gmail.com>
2021-09-22 00:52:10 +0530

    initial templete ready

--- src/App.js
+++ src/App.js
@@ -1,41 +1,89 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
-
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    advice: '',
+var json = require('./data.json');
+
+const App=()=> {
+  
+  var arr=[];
+  var calculatedFees = 0 ;
+  const [predictedFees, setPredictedFees] = useState(arr);
+  console.log(json);
+  console.log("hila dlaana");
+  var data =json;
+  
+
+
+var count = 0;
+for(var key in json)
+    if(json.hasOwnProperty(key))
+        count++;
+
+console.log(count); 
+  // componentDidMount() {
+  //   this.fetchAdvice();
+  // }
+
+  //  fetchAdvice = async () => {
+  //   await axios.get('https://api.adviceslip.com/advice')
+  //     .then((response) => {
+  //       const { advice } = response.data.slip;
+
+  //       this.setState({ advice });
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // }
+
+  const feesOption=()=>{
+      return{
+        // `<option value="volvo">Volvo</option>`
+      }
   }
-
-  componentDidMount() {
-    this.fetchAdvice();
-  }
-
-   fetchAdvice = async () => {
-    await axios.get('https://api.adviceslip.com/advice')
-      .then((response) => {
-        const { advice } = response.data.slip;
-
-        this.setState({ advice });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  render() {
+  
     return (
       <div className="app">
-        <div className="card">
+        {/* <div className="card">
           <h1 className="heading">{this.state.advice}</h1>
           <button className="button" onClick={this.fetchAdvice}>
             <span>GIVE ME ADVICE!</span>
           </button>
-        </div>
+        </div> */}
+
+
+        <form className="fees-form">
+
+          <label className="fees-form__label" >Fees:     
+          <select name="fees" id="fees">
+            
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option>
+          </select>
+          </label>
+
+          <label className="fees-form__label">Nationality: 
+          <input className="input" type = "number" placeholder ="Enter Fees" />
+          </label>
+
+          <label className="fees-form__label">Courses: 
+          <input className="input" type = "number" placeholder ="Enter Fees" />
+          
+          </label>
+          <label className="fees-form__label">Levels: 
+          <input className="input" type = "number" placeholder ="Enter Fees" />
+          </label>
+       
+      </form>
+
+        <p> Your Fees is : {calculatedFees}</p>
+
       </div>
     );
-  }
+  
 }
 
 export default App;
